259c0e5cecb11f58b9f68fc6885c546dab9b7737
Florin Dragoi <florin.dragoi@scriptics.ro>
2025-02-16 16:46:56 +0200

    refactor: remove unnecessary validation decorators from GetBookWithBreadcrumbDto

--- src/books/dto/get-book-with-breadcrumb.dto.ts
+++ src/books/dto/get-book-with-breadcrumb.dto.ts
@@ -1,30 +1,22 @@
-import { IsString, IsInt, IsOptional } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class GetBookWithBreadcrumbDto {
 
     @ApiProperty({ description: 'The ID of the book' })
-    @IsInt()
     id: number;
 
     @ApiProperty({ description: 'The name of the book' })
-    @IsString()
     name: string;
 
     @ApiProperty({ description: 'The author of the book' })
-    @IsString()
     author: string;
 
     @ApiProperty({ description: 'The category ID of the book' })
-    @IsInt()
     categoryId: number;
 
     @ApiProperty({ description: 'A brief description of the book', required: false })
-    @IsOptional()
-    @IsString()
     description?: string;
 
     @ApiProperty({ description: 'A list of breadcrumb strings showing the category path to the book, starting from the top-level category and including all nested subcategories, separated by ">".'  })
-    @IsString()
     breadcrumbs: string[];
 }
