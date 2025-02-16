7a16c1a4eb1a9f15ca8bd38ac84218e8992db05b
Florin Dragoi <florin.dragoi@scriptics.ro>
2025-02-16 22:47:31 +0200

    refactor: remove DatabaseModule import from AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
 import { BooksModule } from './books/books.module';
 import { CategoriesModule } from './categories/categories.module';
-import {DatabaseModule} from "./database/database.module";
 
 @Module({
   imports: [
     BooksModule,
     CategoriesModule,
-    DatabaseModule,
 
   ],
   controllers: [],
